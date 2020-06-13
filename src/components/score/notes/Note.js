be2c2cf9255c6996c60746be32d22a8ea5ebc85e
Matthew O'Boyle <oboyle.matthew@duke.edu>
2020-06-13 20:16:18 +0100

    Adding eighth and sixteenth notes

--- src/components/score/notes/Note.js
+++ src/components/score/notes/Note.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
+import eighthUpStemFlagSVG from "./note_types/generic_notes/flags/eighth_up_stem_flag.svg";
+import eighthDownStemFlagSVG from "./note_types/generic_notes/flags/eighth_down_stem_flag.svg";
+import sixteenthUpStemFlagSVG from "./note_types/generic_notes/flags/sixteenth_up_stem_flag.svg";
+import sixteenthDownStemFlagSVG from "./note_types/generic_notes/flags/sixteenth_down_stem_flag.svg";
+
 // import NoteHead from "./note_types/generic_notes/NoteHead";
 
 //Head: {Rotate: true, circle: true, text: text, background: }
@@ -7,29 +12,58 @@ import styled from "styled-components";
 // Width & Height
 //Stem: {direction: up/down, tailType: single/connected/none, numTails: 2, connectedTo: +3}
 
+
+
 function Note(props) {
     const { type, label, gapBetweenLines, noteName, stem, lineWidth } = props;
     const stemHeight = 3*gapBetweenLines;
     const borderWidth = lineWidth*1.5;
+    const noteWidth = gapBetweenLines;
+    const noteHeight = gapBetweenLines;
     let background = 'black';
-    if (type === 'half' || type === 'full') {
+    if (type === 'half' || type === 'whole') {
         background = 'white';
     }
     let noteStem;
-    if (stem === 'up') {
-        noteStem = <UpNoteStem background={background} borderWidth={borderWidth} stemHeight={stemHeight} stemWidth={borderWidth} noteWidth={gapBetweenLines} noteHeight={gapBetweenLines}/>
-    } else if (stem === 'down') {
-        noteStem = <DownNoteStem background={background} borderWidth={borderWidth} stemHeight={stemHeight} stemWidth={borderWidth} noteWidth={gapBetweenLines} noteHeight={gapBetweenLines}/>
+    let noteFlag;
+    let src;
+    if (type !== 'whole') {
+        if (stem === 'up') {
+            noteStem = <UpNoteStem background={background} borderWidth={borderWidth} stemHeight={stemHeight} stemWidth={borderWidth} noteWidth={noteWidth} noteHeight={noteHeight}/>
+            if (type === 'eighth' || type === 'sixteenth') {
+                src = (type === 'eighth') ? eighthUpStemFlagSVG : sixteenthUpStemFlagSVG;
+                noteFlag = <Flag stemHeight={stemHeight} topOffset={noteHeight/2 - stemHeight} src={src} leftOffset={borderWidth*2}  />
+            }
+        } else if (stem === 'down') {
+            noteStem = <DownNoteStem background={background} borderWidth={borderWidth} stemHeight={stemHeight} stemWidth={borderWidth} noteWidth={noteWidth} noteHeight={noteHeight}/>
+            if (type === 'eighth' || type === 'sixteenth') {
+                src = (type === 'eighth') ? eighthDownStemFlagSVG : sixteenthDownStemFlagSVG;
+                noteFlag = <Flag stemHeight={stemHeight} topOffset={noteHeight / 2} src={src} leftOffset={0}/>
+            }
+        }
     }
     return (
         <NoteContainer>
             <TextNoteHead background={background} gapBetweenLines={gapBetweenLines} borderWidth={3} >
                 {label && noteName}
             </TextNoteHead>
-            {noteStem}
+            <Stem>
+                {noteStem}
+                {noteFlag}
+            </Stem>
+
         </NoteContainer>
     )
 }
+
+const Flag = styled.img`
+    position: absolute;
+    height: ${props => props.stemHeight}px;
+    // background: yellow;
+    top: ${props => props.topOffset}px;
+    left: ${props => props.leftOffset}px;
+    z-index: 80;
+`;
 
 const TextNoteHead = styled.div`
     position: absolute;
@@ -64,7 +98,10 @@ const UpNoteStem = styled(NoteStem)`
 `;
 
 const NoteContainer = styled.div`
+`
 
+const Stem = styled.div`
+    position: absolute;
 `
 
 export default Note;
